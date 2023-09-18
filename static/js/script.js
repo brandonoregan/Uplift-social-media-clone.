1d5fa3a11ed395ea11761c03755ef973110f6c02
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-09-18 11:39:21 +0100

    fix bug comment toggle feature

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,21 @@
 "use strict";
 
-// const editCommentForm = document.querySelector(".editCommentForm");
-// const exposeEdit = document.querySelector(".exposeEdit");
-const commentDisplay = document.getElementById("commentDisplay");
-const newComForm = document.getElementById("newComForm");
-const postFormContainer = document.getElementById("postFormContainer");
-const rightColHome = document.getElementById("rightColHome");
-const showPostFormButton = document.getElementById("showPostFormButton");
+const commentDisplayButtons = document.querySelectorAll(
+  ".commentDisplayButtons"
+);
 
-// exposeEdit.addEventListener("click", function () {
-//   editCommentForm.classList.toggle("hidden");
-// });
-// Create function that targets closest expose edit and performs toggle above
+// Toggle hidden on all new com buttons
+commentDisplayButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    // Find the closest parent element with the class "postCards"
+    const postCard = button.closest(".postCards");
 
-commentDisplay.addEventListener("click", function () {
-  newComForm.classList.toggle("hidden");
-});
+    // Find the child element with the class "newComForm" within the postCard
+    const newComForm = postCard.querySelector(".newComForm");
 
-showPostFormButton.addEventListener("click", function () {
-  postFormContainer.classList.toggle("hidden");
+    // Toggle the "hidden" class for the found newComForm element
+    newComForm.classList.toggle("hidden");
+
+    // newComFormClass.classList.toggle("hidden");
+  });
 });
