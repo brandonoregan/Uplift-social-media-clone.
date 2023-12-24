1d7e66fa5f99a59d7c3cd3cfe6c088c0606ac4aa
Brandon O'Regan <100802480+brandonoregan@users.noreply.github.com>
2023-12-24 12:36:27 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 # Config object to store and use config settings
 class Config:
     # SECRET_KEY = os.environ.get("DRESSME_SECRET_KEY")
-    SECRET_KEY = "nYCc1dL7mOhjvafDWRI8q37dqlYSxMB5"
+    SECRET_KEY = "Mgo7rZa7Eac4dCXGVEYcM64uscUkpHCr"
     # SQLALCHEMY_DATABASE_URI = os.environ.get("DRESSME_DATABASE_URL")
-    SQLALCHEMY_DATABASE_URI = "postgresql://dressme_db_user:nYCc1dL7mOhjvafDWRI8q37dqlYSxMB5@dpg-cjl00vtk5scs73dincd0-a.oregon-postgres.render.com/dressme_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://uplift_social_media_clone_user:Mgo7rZa7Eac4dCXGVEYcM64uscUkpHCr@dpg-cm41tlq1hbls73aajhhg-a.oregon-postgres.render.com/uplift_social_media_clone"
